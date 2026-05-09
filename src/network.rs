27c3ec694068b2d6fbcc329793beefc8b9600e1b
Thomas Jensen <thomas.jensen_@outlook.com>
2026-05-09 21:05:32 +0200

    perf: implement Rc<Node> for Network

--- src/network.rs
+++ src/network.rs
@@ -1,6 +1,7 @@
 use std::{
     collections::{HashMap, HashSet},
     fmt::{self, Display},
+    rc::Rc,
 };
 
 use crate::{
@@ -12,26 +13,36 @@ use crate::{
 /// for any given node is represented as a [`HashSet`].
 #[derive(Debug, Clone)]
 pub struct Network {
-    pub graph: HashMap<Node, HashSet<Node>>,
+    nodes: HashMap<String, Rc<Node>>,
+    pub graph: HashMap<Rc<Node>, HashSet<Rc<Node>>>,
 }
 
 impl Network {
     /// Add a [`Node`] to the Network.
-    fn add_node(&mut self, node: &Node) {
-        self.graph.entry(node.clone()).or_insert_with(HashSet::new);
+    fn add_node(&mut self, node: &str) -> Rc<Node> {
+        if let Some(existing) = self.nodes.get(node) {
+            Rc::clone(existing)
+        } else {
+            let rc = Rc::new(Node(node.to_string()));
+            self.nodes.insert(node.to_string(), Rc::clone(&rc));
+            self.graph
+                .entry(Rc::clone(&rc))
+                .or_insert_with(HashSet::new);
+            rc
+        }
     }
 
     /// Connect a [`Link`] inside of the Network.
-    fn connect(&mut self, a: &Node, b: &Node) {
+    fn connect(&mut self, a: &Rc<Node>, b: &Rc<Node>) {
         self.graph
-            .entry(a.clone())
+            .entry(Rc::clone(a))
             .or_insert_with(HashSet::new)
-            .insert(b.clone());
+            .insert(Rc::clone(b));
 
         self.graph
-            .entry(b.clone())
+            .entry(Rc::clone(b))
             .or_insert_with(HashSet::new)
-            .insert(a.clone());
+            .insert(Rc::clone(a));
     }
 
     /// Disconnect a [`Link`] from the Network.
@@ -45,27 +56,30 @@ impl Network {
     }
 
     /// Check if two Nodes are neighbors.
-    fn neighbors(&self, node: &Node) -> Option<&HashSet<Node>> {
+    fn neighbors(&self, node: &Node) -> Option<&HashSet<Rc<Node>>> {
         self.graph.get(node)
     }
 
     /// Apply an [`Event`] to the Network.
     pub fn apply_event(&mut self, event: &Event) -> EventEffect {
+        let node = &self.add_node(&event.node.0);
+        let target = &self.add_node(&event.target.0);
+
         match event.event_type {
             EventType::LinkDown => {
-                self.disconnect(&event.node, &event.target);
+                self.disconnect(node, target);
                 EventEffect::TopologyChanged {
                     event: event.clone(),
                 }
             }
             EventType::LinkUp => {
-                self.connect(&event.node, &event.target);
+                self.connect(node, target);
                 EventEffect::TopologyChanged {
                     event: event.clone(),
                 }
             }
             EventType::NodeUnreachable => {
-                let reachable = self.can_reach(&event.node, &event.target);
+                let reachable = self.can_reach(node, target);
                 EventEffect::Observation {
                     event: event.clone(),
                     reachable,
@@ -76,14 +90,19 @@ impl Network {
     }
 
     /// Check if a [`Node`] can reach another [`Node`].
-    pub fn can_reach(&self, node: &Node, target: &Node) -> bool {
+    pub fn can_reach(&self, node: &Rc<Node>, target: &Rc<Node>) -> bool {
         let mut visited = HashSet::new();
         self.traverse(node, target, &mut visited)
     }
 
     /// Recursively traverse the Network using depth-first search, returning
     /// `true` if the target [`Node`] is reachable from the given [`Node`].
-    fn traverse(&self, node: &Node, target: &Node, visited: &mut HashSet<Node>) -> bool {
+    fn traverse(
+        &self,
+        node: &Rc<Node>,
+        target: &Rc<Node>,
+        visited: &mut HashSet<Rc<Node>>,
+    ) -> bool {
         if node == target {
             return true;
         }
@@ -107,15 +126,18 @@ impl Network {
 impl From<Graph> for Network {
     fn from(graph: Graph) -> Self {
         let mut network: Self = Self {
+            nodes: HashMap::new(),
             graph: HashMap::new(),
         };
 
         for node in &graph.nodes {
-            network.add_node(node)
+            let _ = network.add_node(&node.0);
         }
 
         for Link(a, b) in &graph.links {
-            network.connect(a, b);
+            let a = network.add_node(&a.0);
+            let b = network.add_node(&b.0);
+            network.connect(&a, &b);
         }
 
         network
